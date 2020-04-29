470820f13269a04fe895a8b2b7e092d4dcc09868
AlanisYates <yatesalanis@gmail.com>
2020-04-29 00:57:34 -0400

    Addingi n the pages and styling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var menu = document.getElementById("menu");
+// // Get the navbar
+// var menu = document.getElementById("menu");
 
-// Get the offset position of the navbar
-var sticky = menu.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = menu.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    menu.classList.add("sticky")
-  } else {
-    menu.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     menu.classList.add("sticky")
+//   } else {
+//     menu.classList.remove("sticky");
+//   }
+// }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 
