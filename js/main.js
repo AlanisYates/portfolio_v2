307116c2b43addfa2b3a1deaa2ac237fc6c94f0d
AlanisYates <yatesalanis@gmail.com>
2020-04-29 23:41:09 -0400

    styling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // FOr the Filtering
 
 filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
+function filterSelection(c) {  
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    w3RemoveClass(x[i], "show fade-in");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show fade-in");
   }
 }
 
