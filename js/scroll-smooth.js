7264e9835dacfe3fd02deb144f6359b098fe7d60
AlanisYates <yatesalanis@gmail.com>
2020-04-29 23:01:33 -0400

    Adding in a smart nav

--- js/scroll-smooth.js
+++ js/scroll-smooth.js
@@ -1,24 +1,41 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
   
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 600, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      $('a').each(function () {
+          $(this).removeClass('active-menu');
+      })
+      $(this).addClass('active-menu');
+    
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top-20
+      }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
   });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop()+30;
+  $('#menu a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('#menu ul li a').removeClass("active-menu");
+        currLink.addClass("active-menu");
+      }
+      else{
+          currLink.removeClass("active-menu");
+      }
+  });
+}
+
+
